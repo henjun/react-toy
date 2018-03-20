a391db83450879262da46e6dff306c1196ff695f
CoTaKm <kane7274@gmail.com>
2018-03-20 21:10:43 +0900

    react checking flow main

--- app/pages/Sample/index.js
+++ app/pages/Sample/index.js
@@ -1,14 +1,24 @@
+// @flow 
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 import Sample from '../../containers/Sample';
 import store from '../../containers/Sample/store';
 import { Provider } from 'react-redux';
 
+/**
+ * @flow/issues/1472
+ */
+
+const Root = document.getElementById('root');
+if (Root == null) {
+    throw new Error("no Root element");
+}
+
 render(
     <Provider store={store}>
         <Sample />
     </Provider>,
-    document.getElementById("root")
+    Root
 );
 
 if (module.hot){
