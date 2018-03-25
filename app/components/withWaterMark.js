c1d48e4e70d6e69f9aac596e8f9f16ce4b6c90b7
CoTaKm <kane7274@gmail.com>
2018-03-25 20:09:56 +0900

    modify  - Hover over the watermark to hide it

--- app/components/withWaterMark.js
+++ app/components/withWaterMark.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { style } from 'typestyle';
 
 function withWaterMark(WrappedComponent){
+
+
     const waterMarkCss = style({
         position: "absolute",
         opacity: "0.25",
@@ -11,13 +13,36 @@ function withWaterMark(WrappedComponent){
         background: '#ccc'
     });
 
-    return function(props){
-        return (
-            <div>
-                <div className={waterMarkCss}>{WrappedComponent.name}</div>
-                <WrappedComponent {...props} />
-            </div>
-        )
+    return class extends Component {
+        state = {
+            visible: true
+        }
+
+        onMouseEnter(){
+            this.setState({
+                visible: false
+            });
+        }
+
+        onMouseLeave(){
+            this.setState({
+                visible: true
+            });
+        }
+        render(){
+            return this.props.waterMark ? (
+                <div
+                    onMouseLeave={e => this.onMouseLeave(e)}
+                >
+                    <div 
+                        onMouseEnter={e => this.onMouseEnter(e)}
+                        className={waterMarkCss} style={{display: this.state.visible? 'block': 'none'}}>{WrappedComponent.name}</div>
+                    <WrappedComponent {...this.props} />
+                </div>
+            ) : (
+                <WrappedComponent {...this.props} />
+            )
+        }
     }
 }
 export default withWaterMark;
